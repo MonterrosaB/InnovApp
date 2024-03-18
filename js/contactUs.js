d9ec48eb11f674a77ce05167d0284cf727414622
Gerardo <20210490@ricaldone.edu.sv>
2024-03-18 03:27:33 -0600

    Formulario Funcional

--- js/contactUs.js
+++ js/contactUs.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const progressBar = document.getElementById("progressBar");
     const progressHandle = document.getElementById("progressHandle");
     const budgetAmount = document.getElementById("budgetAmount");
-    const minBudget = 10000;
+    const minBudget = 1000;
     const maxBudget = 100000;
   
     function updateProgressBar(width) {
@@ -32,5 +32,31 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   
     progressHandle.addEventListener("mousedown", handleMouseDown);
-  });
+});
   
+
+const formulario = document.getElementById('formularioContacto');
+const enviarBtn = document.getElementById('enviarBtn');
+
+formulario.addEventListener('submit', function(event)
+{
+    event.preventDefault();
+
+    const nombreInput = document.getElementById('nombre')
+    const nombreValor = nombreInput.value;
+    const motivo = document.getElementById("motivo")
+    const motivoValor = motivo.value;
+    const mensaje = document.getElementById("mensaje")
+    const mensajeValor = mensaje.value;
+    const correo = document.getElementById("correo")
+    const correoValor = correo.value;
+
+    Swal.fire({
+      icon: 'success',
+      title: 'Mensaje Enviado!',
+      text: `Gracias por contactarnos ${nombreValor}, Tu mensaje ha sido enviado correctamente`
+    });
+
+    formulario.reset();
+
+});
